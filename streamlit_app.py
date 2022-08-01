10d5319212c2381dead30014ca6bcc3084752ba4
Carlos Eduardo Uchôa Brandão Memória <kdumemoria@gmail.com>
2022-08-01 18:46:54 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,55 +1,69 @@
-# importando as bibliotecas
+#Import the required Libraries
 import streamlit as st
-import matplotlib as plt
 import pandas as pd
-import seaborn as sns
+import matplotlib.pyplot as plt
+
 import plotly.express as px
-from PIL import Image
 
-df = pd.read_parquet('https://drive.google.com/u/0/uc?id=1HXq9mczY-5OpFaXK3kk8zAgFEgEgF3jt&export=download')
-st.dataframe(df)
+st.set_page_config(layout="wide")
 
-#Distribuição dos dados da classe Y (Não existe ocorrência = n, Existe ocorrência = e)
+# Functions for each of the pages
+def home(uploaded_file):
+    if uploaded_file:
+        st.header('Begin exploring the data using the menu on the left')
+    else:
+        st.header('To begin please upload a file')
 
-n = pd.value_counts(df['target']) [0]
-e = pd.value_counts(df['target']) [1]
+def data_summary():
+    st.header('Statistics of Dataframe')
+    st.write(df.describe())
 
-condTrue = 'Não há ocorrência do evento que desejamos prever em '
-condFalse = 'Existe ocorrência do evento que desejamos prever '
-resposta = 'linhas'
+def data_header():
+    st.header('Header of Dataframe')
+    st.write(df.head())
 
-tam = len(df)
-
-pie = pd.DataFrame([['Há ocorrência',n],['Não há ocorrência',e]],columns=['Target' , 'Quant'])
-
-def pie_chart(data,col1,col2,title): 
-    labels = {'Não':0,'Sim':1}
-    sizes = data[col2]
-    colors = ['#e5ffcc', '#f80032']
-
-    plt.pie(sizes, labels=labels, colors=colors,
-                autopct='%1.1f%%', shadow=True, startangle=140, labeldistance =1.2)
-    plt.title( title )
+def displayplot():
+    st.header('Plot of Data')
     
-    plt.axis('equal')
-    plt.show()
+    fig, ax = plt.subplots(1,1)
+    ax.scatter(x=df['Depth'], y=df['Magnitude'])
+    ax.set_xlabel('Depth')
+    ax.set_ylabel('Magnitude')
+    
+    st.pyplot(fig)
 
-pie_chart(pie,'Target' , 'Quant','Distribuição Percentual quanto a existência ou não de ocorrência')
+def interactive_plot():
+    col1, col2 = st.columns(2)
+    
+    x_axis_val = col1.selectbox('Select the X-axis', options=df.columns)
+    y_axis_val = col2.selectbox('Select the Y-axis', options=df.columns)
 
-print("\n")
-print("{}{}{}".format(condTrue,n," "+ resposta))
-print("{}{}{}".format(condFalse,e," "+ resposta))
-print("\n")
+    plot = px.scatter(df, x=x_axis_val, y=y_axis_val)
+    st.plotly_chart(plot, use_container_width=True)
 
-plt.bar(pie.Target,pie.Quant, color = ['#e5ffcc', '#f80032'])
-plt.title("Distribuição quanto a existência ou não de ocorrência")
-plt.xlabel("Existe ocorrência?")
-plt.ylabel('Quantidade de Registros')
-plt.show()
-#Tipos de variaveis
-fig = px.bar(x = [6,5],
-            y = ['Categóricas','Numéricas'],
-            orientation='h', title=" Tipos de dados ",
-             labels={'x':'Target','y':'Quant'},width=800, height=400)
-st.plotly_chart(fig)
-st = pie.plot(kind='hist')
+# Add a title and intro text
+st.title('Earthquake Data Explorer')
+st.text('This is a web app to allow exploration of Earthquake Data')
+
+# Sidebar setup
+st.sidebar.title('Sidebar')
+upload_file = st.sidebar.file_uploader('Upload a file containing earthquake data')
+#Sidebar navigation
+st.sidebar.title('Navigation')
+options = st.sidebar.radio('Select what you want to display:', ['Home', 'Data Summary', 'Data Header', 'Scatter Plot', 'Fancy Plots'])
+
+# Check if file has been uploaded
+if upload_file is not None:
+    df = pd.read_parquet(upload_file)
+
+# Navigation options
+if options == 'Home':
+    home(upload_file)
+elif options == 'Data Summary':
+    data_summary()
+elif options == 'Data Header':
+    data_header()
+elif options == 'Scatter Plot':
+    displayplot()
+elif options == 'Interactive Plots':
+    interactive_plot()
