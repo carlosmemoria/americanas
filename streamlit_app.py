a65ce7f89f13b76758cb2f25bde92f999a7fe2da
Carlos Eduardo Uchôa Brandão Memória <kdumemoria@gmail.com>
2022-08-01 19:33:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,7 @@ import plotly.express as px
 st.set_page_config(layout="wide")
 n = pd.value_counts(df['target']) [0]
 e = pd.value_counts(df['target']) [1]
-n = pd.value_counts(df['target']) [0]
-e = pd.value_counts(df['target']) [1]
+
 # Functions for each of the pages
 def home(uploaded_file):
     if uploaded_file:
@@ -29,48 +28,30 @@ def displayplot():
     st.header('Plot of Data')
     
     fig, ax = plt.subplots(1,1)
-    ax.scatter(x=n, y=e)
-    ax.set_xlabel('n')
-    ax.set_ylabel('e')
+    ax.scatter(x=df['Depth'], y=df['Magnitude'])
+    ax.set_xlabel('Depth')
+    ax.set_ylabel('Magnitude')
     
     st.pyplot(fig)
-
+n = pd.value_counts(df['target']) [0]
+e = pd.value_counts(df['target']) [1]
 def graf():
-	#Distribuição dos dados da classe Y (Não existe ocorrência = n, Existe ocorrência = e)
 
+pie = pd.DataFrame([['Há ocorrência',n],['Não há ocorrência',e]],columns=['Target' , 'Quant'])
 
-	condTrue = 'Não há ocorrência do evento que desejamos prever em '
-	condFalse = 'Existe ocorrência do evento que desejamos prever '
-	resposta = 'linhas'
+def pie_chart(data,col1,col2,title): 
+    labels = {'Não':0,'Sim':1}
+    sizes = data[col2]
+    colors = ['#e5ffcc', '#f80032']
 
-	tam = len(df)
+    plt.pie(sizes, labels=labels, colors=colors,
+                autopct='%1.1f%%', shadow=True, startangle=140, labeldistance =1.2)
+    plt.title( title )
+    
+    plt.axis('equal')
+    plt.show()
 
-	pie = pd.DataFrame([['Há ocorrência',n],['Não há ocorrência',e]],columns=['Target' , 'Quant'])
-
-	def pie_chart(data,col1,col2,title): 
-		labels = {'Não':0,'Sim':1}
-		sizes = data[col2]
-		colors = ['#e5ffcc', '#f80032']
-
-		plt.pie(sizes, labels=labels, colors=colors,
-					autopct='%1.1f%%', shadow=True, startangle=140, labeldistance =1.2)
-		plt.title( title )
-		
-		plt.axis('equal')
-		plt.show()
-
-	pie_chart(pie,'Target' , 'Quant','Distribuição Percentual quanto a existência ou não de ocorrência')
-
-	print("\n")
-	print("{}{}{}".format(condTrue,n," "+ resposta))
-	print("{}{}{}".format(condFalse,e," "+ resposta))
-	print("\n")
-
-	plt.bar(pie.Target,pie.Quant, color = ['#e5ffcc', '#f80032'])
-	plt.title("Distribuição quanto a existência ou não de ocorrência")
-	plt.xlabel("Existe ocorrência?")
-	plt.ylabel('Quantidade de Registros')
-	plt.show()
+pie_chart(pie,'Target' , 'Quant','Distribuição Percentual quanto a existência ou não de ocorrência')
 
 def interactive_plot():
     col1, col2 = st.columns(2)
