4208cf033ce9a59b806fad262e019b3d9568b5ef
Carlos Eduardo Uchôa Brandão Memória <kdumemoria@gmail.com>
2022-08-01 21:25:24 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,11 +25,12 @@ def data_header():
 def displayplot():
     st.header('Plot of Data') 
     
-    #fig = px.bar(y = [ df['target'].value_counts() [0], df['target'].value_counts() [1] ],
-    fig = px.bar(train, y=[ df['target'].value_counts() [0], df['target'].value_counts() [1] ], x=['Não atende','Atende'], #color="Sex", 
-		 title=" Sobreviventes x Sexo ",
-		 labels={'Sex': 'Sexo', 'Survived': 'Sobreviventes'},width=400, height=400)
-    st.plotly_chart(fig, use_container_width=False, sharing='streamlit')
+    fig = px.bar(x = [ df['target'].value_counts() [0], df['target'].value_counts() [1] ],
+    y = ['Não atende','Atende'],
+    orientation='h', title=" Tipos de dados ",
+    labels={'x':'Quantidade','y':'Condição'},width=800, height=400)
+
+    st.plotly_chart(fig)
 
 def bar_plot():
 
