e53534f579254b3a12960dbc0099e040e83e73f7
Carlos Eduardo Uchôa Brandão Memória <kdumemoria@gmail.com>
2022-08-01 21:34:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,15 +23,17 @@ def data_header():
     st.write(df.head())
 
 def displayplot():
-    st.header('Plot of Data') 
-    col1 = st.beta_columns(1)
-    with col1 :	
-	    fig = px.bar(x = ['Não atende','Atende'],
-	    y = [ df['target'].value_counts() [0], df['target'].value_counts() [1] ],
-	    orientation='h', title=" Tipos de dados ",
-	    labels={'x':'Quantidade','y':'Condição'},width=800, height=400)
+    st.header('Gráfico de Pizza') 
+    labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+    sizes = [15, 30, 45, 10]
+    explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
-    st.plotly_chart(fig)
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+   	shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    st.pyplot(fig1)
 
 def bar_plot():
 
